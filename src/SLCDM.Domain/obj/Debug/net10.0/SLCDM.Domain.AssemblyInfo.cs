6c9bb8313676b9a296bb29124d2227d1eb008549
agrijalva-2024337 <agrijalva-2024337@kinal.edu.gt>
2026-08-26 16:58:55 -0600

    feat: Se agrego la entidad Area en SLCDM.Domain

--- src/SLCDM.Domain/obj/Debug/net10.0/SLCDM.Domain.AssemblyInfo.cs
+++ src/SLCDM.Domain/obj/Debug/net10.0/SLCDM.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645a86d3f0a53d83ef5a97ad9d555b521f1fb0aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eacc56192b2b55a4b6abf393e621492afaf60a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
