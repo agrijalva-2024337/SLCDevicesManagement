06fb4ae5adef5282a919a918aac211048b58dde9
agrijalva-2024337 <agrijalva-2024337@kinal.edu.gt>
2026-08-27 09:52:48 -0600

    Ignorar carpetas de compilación bin y obj

--- src/SLCDM.Domain/obj/Debug/net10.0/SLCDM.Domain.AssemblyInfo.cs
+++ src/SLCDM.Domain/obj/Debug/net10.0/SLCDM.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356c26d6fac999e1a59a6a854ca4f73e68374290")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4974ddd0f6edbaf9f94cc5820610ea640b9681ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SLCDM.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
