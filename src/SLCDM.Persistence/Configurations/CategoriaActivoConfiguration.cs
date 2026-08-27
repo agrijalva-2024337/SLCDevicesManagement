0004152860f6172e9bd329e40148782233af5606
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-08-27 17:58:16 -0600

    feat: agregar configuracion Fluent API de Categoria_Activo

---
+++ src/SLCDM.Persistence/Configurations/CategoriaActivoConfiguration.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SLCDM.Domain.Entities;
+
+namespace SLCDM.Persistence.Configurations;
+
+public class CategoriaActivoConfiguration : IEntityTypeConfiguration<CategoriaActivo>
+{
+    public void Configure(EntityTypeBuilder<CategoriaActivo> builder)
+    {
+        builder.ToTable("Categoria_Activo");
+
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).HasColumnName("id_categoria");
+
+        builder.Property(c => c.Habilitado)
+            .HasColumnName("habilitado")
+            .HasDefaultValue(true);
+
+        builder.Property(c => c.Nombre)
+            .HasColumnName("nombre")
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+
+        builder.Property(c => c.Descripcion)
+            .HasColumnName("descripcion")
+            .HasColumnType("varchar(200)");
+
+        builder.HasIndex(c => c.Nombre).IsUnique();
+    }
+}
