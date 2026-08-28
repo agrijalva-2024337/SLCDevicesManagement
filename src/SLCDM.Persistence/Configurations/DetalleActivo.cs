f203b4f98be4e4fcae65ec9d4c103b40dba27a49
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-08-28 11:08:43 -0600

    feat: agregar configuracion Fluent API de Detalle_Activo

---
+++ src/SLCDM.Persistence/Configurations/DetalleActivo.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SLCDM.Domain.Entities;
+
+namespace SLCDM.Persistence.Configurations;
+
+public class DetalleActivoConfiguration : IEntityTypeConfiguration<DetalleActivo>
+{
+    public void Configure(EntityTypeBuilder<DetalleActivo> builder)
+    {
+        builder.ToTable("detalle_activo");
+
+        builder.HasKey(d => d.Id);
+        builder.Property(d => d.Id).HasColumnName("id_detalle_activo");
+
+        builder.Property(d => d.IdActivo).HasColumnName("id_activo").IsRequired();
+        builder.Property(d => d.IdHistoricoInventario).HasColumnName("id_historico_inventario").IsRequired();
+
+        builder.Property(d => d.Encontrado)
+            .HasColumnName("encontrado")
+            .IsRequired();
+
+        builder.Property(d => d.BuenEstado)
+            .HasColumnName("buen_estado")
+            .IsRequired();
+
+        builder.Property(d => d.Observaciones)
+            .HasColumnName("observaciones")
+            .HasColumnType("varchar(300)");
+
+        builder.Property(d => d.FechaVerificacion)
+            .HasColumnName("fecha_verificacion")
+            .HasColumnType("date");
+
+        builder.HasOne(d => d.Activo)
+            .WithMany()
+            .HasForeingKey(d => d.IdActivo)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(d => d.HistoricoInventario)
+            .WithMany()
+            .HasForeignKey(d => d.IdHistoricoInventario)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(d => d.IdActivo);
+        builder.HasIndex(d => d.IdHistoricoInventario);
+    }
+}
