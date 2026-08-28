ff7cca2af3f9078b494530a513bcd9efb592459f
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-08-28 09:59:31 -0600

    feat: agregar configuracion Fluent API de Ubicacion

---
+++ src/SLCDM.Persistence/Configurations/UbicacionConfiguration.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SLCDM.Domain.Entities;
+
+namespace SLCDM.Persistence.Configurations;
+
+public class UbicacionConfiguration : IEntityTypeConfiguration<Ubicacion>
+{
+    public void Configure(EntityTypeBuilder<Ubicacion> builder)
+    {
+        builder.ToTable("ubicacion");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnName("id_ubicacion");
+
+        builder.Property(u => u.Habilitado)
+            .HasColumnName("habilitado")
+            .HasDefaultValue(true);
+
+        builder.Property(u => u.Nombre)
+            .HasColumnName("nombre")
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+
+        builder.Property(u => u.Descripcion)
+            .HasColumnName("descripcion")
+            .HasColumnType("varchar(200)");
+
+        builder.Property(u => u.Latitud)
+            .HasColumName("latitud")
+            .HasColumnType("decimal(9,6)")
+            .IsRequired();
+
+        builder.Property(u => u.Longitud)
+            .HasColumnName("longitud")
+            .HasColumnType("decimal(9,6)")
+            .IsRequired();
+
+        builder.HasIndex(u => u.Nombre);
+    }
+}
