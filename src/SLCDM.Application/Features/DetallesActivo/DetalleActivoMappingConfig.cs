a90b396a84b6231d9ac4de5d3d6b84e4e704dce5
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-09-02 17:43:24 -0600

    fix: arreglo de clases para ejercutar correctamente el proyecto.

--- src/SLCDM.Application/Features/DetallesActivo/DetalleActivoMappingConfig.cs
+++ src/SLCDM.Application/Features/DetallesActivo/DetalleActivoMappingConfig.cs
@@ -8,15 +8,5 @@ public sealed class DetalleActivoMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<DetalleActivo, DetalleActivoDto>();
-
-        config.NewConfig<Commands.CreateDetalleActivoCommand, DetalleActivo>()
-            .Ignore(dest => dest.Id)
-            .Ignore(dest => dest.Activo!)
-            .Ignore(dest => dest.HistoricoInventario!);
-
-        config.NewConfig<Commands.UpdateDetalleActivoCommand, DetalleActivo>()
-            .Ignore(dest => dest.Id)
-            .Ignore(dest => dest.Activo!)
-            .Ignore(dest => dest.HistoricoInventario!);
     }
 }
