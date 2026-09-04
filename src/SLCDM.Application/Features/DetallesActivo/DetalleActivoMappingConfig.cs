a7f43a08a0bba1e07ebf5892b2899a11234e3fc4
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-09-04 16:01:18 -0600

    feat: mapear comando de creacion de detalle de activo a la entidad

--- src/SLCDM.Application/Features/DetallesActivo/DetalleActivoMappingConfig.cs
+++ src/SLCDM.Application/Features/DetallesActivo/DetalleActivoMappingConfig.cs
@@ -8,5 +8,10 @@ public sealed class DetalleActivoMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<DetalleActivo, DetalleActivoDto>();
+
+        config.NewConfig<Commands.CreateDetalleActivoCommand, DetalleActivo>()
+            .Ignore(dest => dest.Id)
+            .Ignore(dest => dest.Activo!)
+            .Ignore(dest => dest.HistoricoInventario!);
     }
 }
